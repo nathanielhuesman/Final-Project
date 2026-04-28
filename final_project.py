7f6812bd0a7fde8e44a7039d0604a1cfffa70ed5
nathanielhuesman <nathaniel.huesman@gmail.com>
2026-04-28 10:10:48 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1K1-VH-kIQSYFobMswYCLS5q26WKhGnfd
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
